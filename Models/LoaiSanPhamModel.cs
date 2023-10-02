d2e666978baea25a6704db4fb93c44ba4713f47e
TimeJacker <anhpewpewsts@gmail.com>
2023-10-02 20:58:32 +0700

    first commit

---
+++ Models/LoaiSanPhamModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Models
+{
+    public class LoaiSanPhamModel
+    {
+        public int MaLoai { get; set; }
+        public string TenLoai { get; set; }
+    }
+}
---
+++ Models/SanPhamModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class SanPhamModel
+    {
+
+        public int MaSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public string Anh { get; set; }
+        public int SoLuong { get; set; }
+        public string Mota { get; set; }
+        public DateTime NgayTao { get; set; }
+        public float Gia { get; set; }
+        public string KichCoManHinh { get; set; }
+        public string DoPhanGiai { get; set; }
+
+
+        //public List<ThongSoKyThuatModel> list_jsonThongSoKyThuat { get; set; }//
+        public LoaiSanPhamModel loaiSanPham { get; set; }
+    }
+
+}
